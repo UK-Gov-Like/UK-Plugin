f0de6d95553d72094fae9a1624f83be47dd49a8c
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2020-10-06 15:55:45 -0300

    Code Format

--- src/main/java/com/github/ukgovlike/ukplugin/api/ExcelAPI.java
+++ src/main/java/com/github/ukgovlike/ukplugin/api/ExcelAPI.java
@@ -2,7 +2,6 @@ package com.github.ukgovlike.ukplugin.api;
 
 import com.github.ukgovlike.ukplugin.api.file.ExcelFileAPI;
 import jxl.Sheet;
-import jxl.Workbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -19,22 +18,40 @@ import java.util.Map;
 
 public class ExcelAPI {
    private FileInputStream fileInputStream;
-   { try {     fileInputStream = new FileInputStream(ExcelFileAPI.file);} catch (FileNotFoundException e) {
-       try {ExcelFileAPI.createFile();} catch (IOException ioException) {ioException.printStackTrace();}}}
+   {
+     try {
+       fileInputStream = new FileInputStream(ExcelFileAPI.file);
+     } catch (FileNotFoundException e) {
+       try {ExcelFileAPI.createFile();
+       } catch (IOException ioException) {
+         ioException.printStackTrace();
+       }
+     }
+   }
    private XSSFWorkbook workbook;
-   {try {workbook = new XSSFWorkbook(fileInputStream);} catch (IOException e) {e.printStackTrace();} }
+   {
+     try {
+       workbook = new XSSFWorkbook(fileInputStream);
+     } catch (IOException e) {
+       e.printStackTrace();
+     }
+   }
 
     public ExcelAPI() {}
 
     public Sheet createSheet(String name) {
-        if(hasSheet(name)) {return null;}
+        if(hasSheet(name)) {
+          return null;
+        }
         return (Sheet) workbook.createSheet(name);
     }
     public boolean hasSheet(String name) {
         return workbook.getSheet(name) != null;
     }
     public void deleteSheet(String name) {
-       if(!hasSheet(name)) {return;}
+       if(!hasSheet(name)) {
+         return;
+       }
         workbook.removeSheetAt(workbook.getSheetIndex(name));
     }
     public XSSFSheet getSheet(String name) {
@@ -45,32 +62,45 @@ public class ExcelAPI {
         Map<Integer, List<String>> data = new HashMap<>();
         int i = 0;
         for (Row row : (XSSFSheet)sheet) {
-            data.put(i, new ArrayList<String>());
-            for (Cell cell : row) {
-             if(cell.getAddress().getColumn() == x && cell.getAddress().getRow() == y) {
-                 return cell;
-             }
+          data.put(i, new ArrayList<String>());
+          for (Cell cell : row) {
+            if(cell.getAddress().getColumn() == x && cell.getAddress().getRow() == y) {
+              return cell;
             }
-            i++;
+          }
+          i++;
         }
         return null;
     }
     public Object readCell(Cell cell) {
        switch (cell.getCellTypeEnum()) {
-           case BOOLEAN: return cell.getBooleanCellValue();
-           case NUMERIC: return cell.getNumericCellValue();
-           default: return cell.getStringCellValue();
+           case BOOLEAN:
+             return cell.getBooleanCellValue();
+           case NUMERIC:
+             return cell.getNumericCellValue();
+           default:
+             return cell.getStringCellValue();
        }
     }
     public void writeCell(Cell cell, Object toWrite) {
        switch (toWrite.getClass().getSimpleName().toLowerCase()) {
-           case "boolean": cell.setCellValue((boolean) toWrite);break;
-           case "string": cell.setCellValue((String)toWrite);break;
-           case "double": cell.setCellValue(Double.parseDouble((String) toWrite));break;
-           case "integer": cell.setCellValue(Integer.parseInt((String) toWrite));break;
-           case "date": cell.setCellValue(Date.parse((String) toWrite)); break;
+           case "boolean":
+             cell.setCellValue((boolean) toWrite);
+             break;
+           case "string":
+             cell.setCellValue((String)toWrite);
+             break;
+           case "double":
+             cell.setCellValue(Double.parseDouble((String) toWrite));
+             break;
+           case "integer":
+             cell.setCellValue(Integer.parseInt((String) toWrite));
+             break;
+           case "date":
+             cell.setCellValue(Date.parse((String) toWrite));
+             break;
+         default:
+           break;
        }
     }
-
-
 }
