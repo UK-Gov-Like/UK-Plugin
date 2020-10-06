df01164bd1cd5e6563878d9dc2897dc35ff32b0d
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2020-10-05 21:39:34 -0300

    Base Plugin

---
+++ src/main/java/com/github/ukgovlike/ukplugin/UKPlugin.java
@@ -0,0 +1,16 @@
+package com.github.ukgovlike.ukplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UKPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
