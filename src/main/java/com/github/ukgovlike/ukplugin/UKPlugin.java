ce284d7b29302c7636e11cce8f02c030c28c2ca7
WizardlyBump17 <davicraft130@gmail.com>
2020-10-06 15:57:20 -0300

    Added a default BankPlayerManager

--- src/main/java/com/github/ukgovlike/ukplugin/UKPlugin.java
+++ src/main/java/com/github/ukgovlike/ukplugin/UKPlugin.java
@@ -1,20 +1,25 @@
 package com.github.ukgovlike.ukplugin;
 
+import com.github.ukgovlike.ukplugin.api.player.manager.BankPlayerManager;
 import com.github.ukgovlike.ukplugin.commands.Bank;
 import com.github.ukgovlike.ukplugin.events.InventoryClick;
+import com.github.ukgovlike.ukplugin.implementation.player.manager.BankPlayerManagerImpl;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class UKPlugin extends JavaPlugin {
 
+    @Getter
+    private static final BankPlayerManager bankPlayerManager = new BankPlayerManagerImpl();
+
     /**
-     * @authors Plugner, LoboMetalurgico
+     * @authors Plugner, LoboMetalurgico, WizardlyBump17
      * @since 05/10/2020
      */
-
     @Override
     public void onEnable() {
-     this.getCommand("bank").setExecutor(new Bank());
-     this.getServer().getPluginManager().registerEvents(new InventoryClick(),this);
+        this.getCommand("bank").setExecutor(new Bank());
+        this.getServer().getPluginManager().registerEvents(new InventoryClick(), this);
     }
 
     @Override
